c57a58629d06926baa401dfc997b57b4c0352b00
OjoJosh7 <111016654+OjoJosh7@users.noreply.github.com>
2023-05-11 11:47:41 +0100

    Add files via upload
    Investment- _str_ method

---
+++ Class_investment.py
@@ -0,0 +1,17 @@
+class Investment:
+    def __init__(self, principal = 0, interest = 0):
+        self.p = principal
+        self.i = interest
+    
+    def __str__(self):
+        return 'Investment value is: ${}:{}'.format(self.profit, self.n)
+    def value_after(self, n = 0):
+        self.n = n
+        self.profit = pow(self.p * (1 + self.i), self.n)
+        return self.profit
+    
+payment = Investment(10000, 7)
+payment.value_after(7)
+print(payment)
+
+

169a058f32b121d39b98731ff252370d8a50aeec
OjoJosh7 <111016654+OjoJosh7@users.noreply.github.com>
2023-05-11 12:05:13 +0100

    Update Class_investment.py

--- Class_investment.py
+++ Class_investment.py
@@ -4,10 +4,10 @@ class Investment:
         self.i = interest
     
     def __str__(self):
-        return 'Investment value is: ${}:{}'.format(self.profit, self.n)
+        return 'Investment value is: ${}, interest rate- {}%'.format(self.profit, self.n)
     def value_after(self, n = 0):
         self.n = n
-        self.profit = pow(self.p * (1 + self.i), self.n)
+        self.profit = pow(self.p * (1 + (self.i/100)), self.n)
         return self.profit
     
 payment = Investment(10000, 7)
